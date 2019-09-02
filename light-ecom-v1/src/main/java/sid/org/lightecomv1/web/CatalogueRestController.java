23b47b36153b1389172c4338d33ae42bc367fff4
amalboukili <boukili.amal@gmail.com>
2019-09-02 14:24:20 +0100

    Partie Back End

--- light-ecom-v1/src/main/java/sid/org/lightecomv1/web/CatalogueRestController.java
+++ light-ecom-v1/src/main/java/sid/org/lightecomv1/web/CatalogueRestController.java
@@ -1,6 +1,7 @@
 package sid.org.lightecomv1.web;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.http.MediaType;
@@ -12,18 +13,22 @@ import sid.org.lightecomv1.dao.ProductRepository;
 import sid.org.lightecomv1.entities.Product;
 
 @RestController
-public class CatalogueRestController{
+public class CatalogueRestController {
+
     private ProductRepository productRepository;
 
     public CatalogueRestController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    @GetMapping(path = "/photoProduct/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    @GetMapping(path = "/photoProduct/{id}", produces =
+    MediaType.IMAGE_PNG_VALUE)
     public byte[] getPhoto(@PathVariable("id") Long id) throws Exception{
-        Product p= productRepository.findById(id).get();
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home"+"/ecom/products/"+p.getName())));
+    Product p= productRepository.findById(id).get();
+    return
+    Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/ecom/products/"+p.getPhotoName()));
     }
 
-    
+   
+
 }
